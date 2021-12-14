8016eef4d6aa42802582bd26c805ddcc3f65cada
LCarpenter87 <L.carpenter@outlook.com>
2021-12-14 17:57:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,6 @@ if st.button("Let's go", key=None, help=None, on_click=None, args=None, kwargs=N
     new_data = np.array(new_data).reshape(1, -1)
     result = logreg.predict_proba(new_data)
     result = round(result[0][0], 0)       
-    st.text(f' Your chance of getting a gift: {result[0][0]}%')
+    st.text(f' Your chance of getting a gift: {result}%')
     st.text(new_data)
 
