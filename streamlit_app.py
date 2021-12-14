04d9648399a359063d4bb963eb106811eb64dae0
LCarpenter87 <L.carpenter@outlook.com>
2021-12-14 14:40:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,4 @@ answers['nice'] = st.radio('How often did you do something nice for your parents
 
 if st.button("Let's go", key=None, help=None, on_click=None, args=None, kwargs=None):
   for x,i in answers.items():
-  st.text(f'{x} : {i}')
+    st.text(f'{x} : {i}')
