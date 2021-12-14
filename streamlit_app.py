8c3f9c7e0c9d36c5d9af0456f13d376d8da7e921
LCarpenter87 <L.carpenter@outlook.com>
2021-12-14 17:42:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,31 +5,28 @@ import pickle
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MinMaxScaler
 
+st.title('Will you get a present from Santa?')
+
 options = {'time_options' :'Every Time,Most of the time,Rarely,Never'.split(','),
            'secondary' : 'Never,Just a few times,Occassionally,Often,All the time'.split(','),
            'chores' : 'I do it first time,They just ask me a couple of times,They have to remind me a lot,I never do my chores'.split(',')}
 
-types = {'pleaseandthanks':'time_options',
-         'disobey':'time_options',
-         'hugs':'secondary',
-         'sharing':'secondary',
-         'chores': 'chores',
-         'nice':'secondary'}
-
-st.title('Will you get a present from Santa?')
+types = {'pleaseandthanks': ('time_options', 'How often did you say Please and Thank you?'),
+         'disobey': ('time_options', 'How often did you disobey your parents?'),
+         'hugs': ('secondary', 'How many times did you hug your parents?'),
+         'sharing': ('secondary', 'How many times did you share?'),
+         'chores': ('chores','How much do your guardians have to nag to get you to do your chores?'),
+         'nice': ('secondary', 'How often did you do something nice for your parents, without being asked?')}
 
 answers = dict()
 
-answers['pleaseandthanks'] = st.radio('How often did you say Please and Thank you?',options[types['pleaseandthanks']])
-answers['disobey'] = st.radio('How often did you disobey your parents?',options[types['disobey']])
-answers['hugs'] = st.radio('How many times did you hug your parents?',options['secondary'])
-answers['sharing'] = st.radio('How many times did you share?',options['secondary'])
-answers['chores'] = st.radio('How much do your guardians have to nag to get you to do your chores?',options['chores'])
-answers['nice'] = st.radio('How often did you do something nice for your parents, without being asked?',options['secondary'])
+for k,v in types.items():
+    answers[k] = st.radio(v[1], options[types[v][0]])
 
-
+## Take in the Text Input
 answers['swearing'] = st.slider('How many times did you swear in 2021?', min_value=0, max_value=1000)
 
+## Bring in the Pickle Files
 logreg = pickle.load(open('santa.sav', 'rb'))
 scaler = pickle.load(open('scaler.sav', 'rb'))
 
